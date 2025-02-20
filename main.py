6ca3163afa8c76526f269febe1d39c86c6852485
1xbet-download <hesham.mohamed9628@gmail.com>
2025-02-20 19:51:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import telebot
 import os
+import sys
 
+# جلب التوكن من متغيرات البيئة
 TOKEN = os.getenv("BOT_TOKEN")
+
+# التحقق من وجود التوكن
+if not TOKEN:
+    print("Error: BOT_TOKEN not found in environment variables.")
+    sys.exit(1)
+
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=['document'])
